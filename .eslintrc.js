c403a84e5d6d9b18fe63e57880e49b4028f36964
HPCL202001004 <hpcl202001004@HPCL202001004deMacBook-Pro.local>
2020-03-11 16:35:35 +0800

    update: 更新eslint的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
-
 module.exports = {
+  root: true,
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ]
 }
