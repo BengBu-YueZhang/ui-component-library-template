73755aa6357e3e8b26b2f3206b77a759768f9323
BengBu-YueZhang <zhangyue_coder@163.com>
2020-04-26 15:41:04 +0800

    feat: 添加lib打包命令，为按需引入做准备

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,24 +1,32 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 const {
   src,
   dest,
   series
-} = require('gulp')
-const clean = require('gulp-clean-css')
-const less = require('gulp-less')
-const rename = require('gulp-rename')
-const autoprefixer = require('gulp-autoprefixer')
+} = require('gulp');
+const clean = require('gulp-clean-css');
+const less = require('gulp-less');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const babelrc = require('../.babelrc.js');
 
-const ENTRY = resolve(__dirname, '../src/styles/index.less')
-const OUTPUT = resolve(__dirname, '../dist/styles')
+const ENTRY = resolve(__dirname, '../src/styles/index.less');
+const OUTPUT = resolve(__dirname, '../dist/styles');
+const LIB_ENTRY = [
+  resolve(__dirname, '../src/**/*.tsx'),
+  resolve(__dirname, '../src/**/*.ts'),
+  resolve(__dirname, '../src/*.tsx')
+];
+const LIB_OUTPUT = resolve(__dirname, '../lib');
 
 const buildCss = () => {
   return src(ENTRY)
   .pipe(less())
   .pipe(autoprefixer())
   .pipe(rename('ui-component-library-template.css'))
-  .pipe(dest(OUTPUT))
-}
+  .pipe(dest(OUTPUT));
+};
 
 const buildUglifyCss = () => {
   return src(ENTRY)
@@ -26,10 +34,20 @@ const buildUglifyCss = () => {
   .pipe(autoprefixer())
   .pipe(clean())
   .pipe(rename('ui-component-library-template.min.css'))
-  .pipe(dest(OUTPUT))
+  .pipe(dest(OUTPUT));
+};
+
+const buildLib = () => {
+  return src(LIB_ENTRY)
+    .pipe(babel(babelrc()))
+    .pipe(dest(LIB_OUTPUT));
 }
 
-exports.default = series(
+exports.style = series(
   buildCss,
   buildUglifyCss
-)
+);
+
+exports.lib = series(
+  buildLib
+);
